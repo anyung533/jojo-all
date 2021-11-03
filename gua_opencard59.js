9972c4baa44b0c06dd52c94dd87a161622cf5bfc
cestjojo <anyung533@users.noreply.github.com>
2021-11-03 11:14:50 +0800

    gua_opencard59.js

--- gua_opencard59.js
+++ gua_opencard59.js
@@ -33,13 +33,13 @@ All变量适用
 ============Quantumultx===============
 [task_local]
 #11.1~11.13 酒水会员盛典
-30 10 1-13 11 * https://raw.githubusercontent.com/smiek2121/scripts/master/gua_opencard59.js, tag=11.1~11.13 酒水会员盛典, enabled=true
+30 11 1-13 11 * gua_opencard59.js, tag=11.1~11.13 酒水会员盛典, enabled=true
 
 */
-let guaopencard_addSku = "false"
-let guaopencard = "false"
+let guaopencard_addSku = "true"
+let guaopencard = "true"
 let guaopenwait = "0"
-let guaopencard_draw = "0"
+let guaopencard_draw = "3"
 
 const $ = new Env('11.1~11.13 酒水会员盛典');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
