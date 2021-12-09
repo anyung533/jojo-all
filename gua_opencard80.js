96c2f9d547c2176bf7244a7e72d6994b26deb826
cestjojo <anyung533@users.noreply.github.com>
2021-12-09 09:11:14 +0800

    gua_opencard80.js

--- gua_opencard80.js
+++ gua_opencard80.js
@@ -36,13 +36,13 @@ cron:30 1 9-15 12 *
 ============Quantumultx===============
 [task_local]
 #12.9~12.15 联合开卡
-30 1 9-15 12 * https://raw.githubusercontent.com/smiek2121/scripts/master/gua_opencard80.js, tag=12.9~12.15 联合开卡, enabled=true
+30 1 9-15 12 * gua_opencard80.js, tag=12.9~12.15 联合开卡, enabled=true
 
 */
-let guaopencard_addSku = "false"
-let guaopencard = "false"
+let guaopencard_addSku = "true"
+let guaopencard = "true"
 let guaopenwait = "0"
-let guaopencard_draw = "0"
+let guaopencard_draw = "3"
 
 const $ = new Env('12.9~12.15 联合开卡-80');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
