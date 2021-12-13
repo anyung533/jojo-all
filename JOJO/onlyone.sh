ebd70c8d18fba0acfc152c565b43129f53778c4d
cestjojo <anyung533@users.noreply.github.com>
2021-12-13 22:32:44 +0800

    onlyone.sh

--- JOJO/onlyone.sh
+++ JOJO/onlyone.sh
@@ -1,6 +1,19 @@
+cd /ql/scripts && pip3 install requests
+cd /ql/scripts && pip3 install --upgrade pip
+cd /ql/scripts && pip3 install jieba
+cd /ql/scripts && pip3 install telethon
+cd /ql/scripts && pip3 install qrcode
+cd /ql/scripts && pip3 install aiohttp
 
-npm install -g pnpm
-pnpm install -g @otplib/preset-default
+
+cd /ql/scripts && apk add libc6-compat
+cd /ql/scripts && apk add nodejs-current
+cd /ql/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev    
+cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev
+
+
+cd /ql/scripts && npm install -g pnpm
+cd /ql/scripts && pnpm install -g @otplib/preset-default
 cd /ql/scripts && pnpm install -g js-base64
 cd /ql/scripts && pnpm install -g fund
 cd /ql/scripts && pnpm install -g jsdom
@@ -17,18 +30,3 @@ cd /ql/scripts && pnpm install -g got@11
 cd /ql/scripts && pnpm install -g ts-node
 cd /ql/scripts && pnpm install -g @types/node
 cd /ql/scripts && pnpm install -g canvas
-
-cd /ql/scripts && pip3 install requests
-cd /ql/scripts && pip3 install --upgrade pip
-cd /ql/scripts && pip3 install aiohttp
-cd /ql/scripts && pip3 install jieba
-
-
-cd /ql/scripts && apk add libc6-compat
-cd /ql/scripts && apk add nodejs-current
-
-
-cd /ql/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
-
-    
-cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev
