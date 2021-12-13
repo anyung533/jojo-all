8b7185c2c0e0fdbbfc2da535ed52d3d9f31b966d
cestjojo <anyung533@users.noreply.github.com>
2021-12-13 15:52:53 +0800

    pull.sh

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
-ql repo https://github.com/LJMX996/jd.git "jd_|jx_|gua_|code_|zy_|jd-task-|jddj_|ql_|ck_" "utils|static|tools|activity|backUp|jd_delCoupon|format_|tmp|cdle|helpjs|tmpcron" "" "aaron" 
+ql repo https://github.com/anyung533/jojo-all.git "jd_|jx_|gua_|code_|zy_|jd-task-|jddj_|ql_|ck_" "utils|static|tools|activity|backUp|jd_delCoupon|format_|tmp|cdle|helpjs|tmpcron" "" "aaron" 
 cp -rfv /ql/repo/anyung533_jojo-all/* /ql/scripts/
 cp -rfv /ql/repo/anyung533_jojo-all/* /ql/scripts/anyung533_jojo-all/
 cp -rfv /ql/repo/anyung533_jojo-all/pull.sh /ql/config/
