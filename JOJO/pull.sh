a91e255534b4bc62a76d911987b7b1a41fcc9f27
cestjojo <46280959+anyung533@users.noreply.github.com>
2021-12-13 14:13:52 +0800

    Add files via upload

---
+++ JOJO/pull.sh
@@ -0,0 +1,15 @@
+#cron 20 * * * * pull.sh
+ql repo https://github.com/anyung533/jojo-all.git "jd_|jx_|gua_|code_|zy_|jd-task-|jddj_|ql_|ck_" "utils|static|tools|activity|backUp|jd_delCoupon|format_|tmp|cdle|helpjs|tmpcron|sillyGirl" 
+
+
+
+cp -rfv /ql/repo/anyung533_jojo-all/* /ql/scripts/
+
+cp -rfv /ql/repo/anyung533_jojo-all/* /ql/scripts/anyung533_jojo-all/
+
+
+
+
+
+
+cp -rfv /ql/repo/anyung533_jojo-all/pull.sh /ql/config/
