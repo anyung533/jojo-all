39098c872a2685659fe43decf1b8de10c4c3e6bc
cestjojo <anyung533@users.noreply.github.com>
2021-12-13 16:38:55 +0800

    cp -rfv /ql/repo/anyung533_jojo-all/* /ql/scripts/ cp -rfv /ql/repo/anyung533_jojo-all/* /ql/scripts/anyung533_jojo-all/ cp -rfv /ql/repo/anyung533_jojo-all/pull.sh /ql/config/

--- JOJO/file.sh
+++ JOJO/file.sh
@@ -1,24 +1,5 @@
-#CK
-cp -rf /ql/repo/anyung533_jojo/ql_refreshCK.py /ql/scripts/
-cp -rf /ql/repo/anyung533_jojo/utils/* /ql/scripts/
-
-
-
-cp -rf /ql/repo/anyung533_jojo/utils/JDJRValidator_Pure.js /ql/scripts/
-#jdCookie.js
-cp -rf /ql/repo/anyung533_jojo/file/jdCookie.js /ql/scripts/anyung533_jojo/
-cp -rf /ql/repo/anyung533_jojo/file/sign_graphics_validate.js /ql/scripts/anyung533_jojo/
-cp -rf /ql/repo/anyung533_jojo/file/ql.js /ql/scripts/anyung533_jojo/
-cp -rf /ql/repo/anyung533_jojo/file/code.sh /ql/config/
-cp -rf /ql/repo/anyung533_jojo/file/task_before.sh /ql/config/
-#助力依赖
-cp -rf /ql/repo/anyung533_jojo/function /ql/scripts/anyung533_jojo/
-
-
-
-
-
-
-
+cp -rfv /ql/repo/anyung533_jojo-all/* /ql/scripts/
+cp -rfv /ql/repo/anyung533_jojo-all/* /ql/scripts/anyung533_jojo-all/
+cp -rfv /ql/repo/anyung533_jojo-all/pull.sh /ql/config/
 
 	
