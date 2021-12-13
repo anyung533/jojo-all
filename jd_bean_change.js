55068c0f069673271af889b350be0328d3ab6e65
cestjojo <anyung533@users.noreply.github.com>
2021-12-14 00:08:17 +0800

    jd_bean_change.js

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -1,5 +1,5 @@
 /*
-cron "0 0-23/3 * * *" jd_bean_change.js, tag:资产变化强化版by-ccwav
+cron "10 12,22 * * *" jd_bean_change.js, tag:资产变化强化版by-ccwav
  */
 
 //详细说明参考 https://github.com/ccwav/QLScript2.
@@ -46,7 +46,7 @@ let IndexGp4 = 0;
 
 let notifySkipList = "";
 let IndexAll = 0;
-let EnableMonth = "true";
+let EnableMonth = "false";
 let isSignError = false;
 let ReturnMessageTitle="";
 //IOS等用户直接用NobyDa的jd cookie
@@ -59,6 +59,7 @@ let llShowMonth = false;
 let Today = new Date();
 let strAllNotify="";
 let llPetError=false;
+let strGuoqi="";
 let RemainMessage = '\n';
 RemainMessage += "⭕提醒:⭕" + '\n';
 RemainMessage += '【极速金币】京东极速版->我的->金币(极速版使用)\n';
@@ -69,13 +70,25 @@ RemainMessage += '【领现金】京东->我的->东东萌宠->领现金(微信�
 RemainMessage += '【东东农场】京东->我的->东东农场,完成是京东红包,可以用于京东app的任意商品\n';
 RemainMessage += '【京喜工厂】京喜->我的->京喜工厂,完成是商品红包,用于购买指定商品(不兑换会过期)\n';
 RemainMessage += '【其他】京喜红包只能在京喜使用,其他同理';
-let BEANCHANGE_PERSENT="170"
+let BEANCHANGE_PERSENT="10"
 
 let WP_APP_TOKEN_ONE = "";
 let TempBaipiao = "";
 if ($.isNode() && process.env.WP_APP_TOKEN_ONE) {
 	WP_APP_TOKEN_ONE = process.env.WP_APP_TOKEN_ONE;
 }
+
+let doExJxBeans ="false";
+let time = new Date().getHours();
+if ($.isNode() && process.env.BEANCHANGE_ExJxBeans=="true") {
+	if (time >= 17){ 
+		console.log(`检测到设定了临期京豆转换喜豆...`);
+		doExJxBeans = process.env.BEANCHANGE_ExJxBeans;
+	} else{
+		console.log(`检测到设定了临期京豆转换喜豆,但时间未到17点后，暂不执行转换...`);
+	}
+}
+
 if ($.isNode() && BEANCHANGE_PERSENT) {
 	intPerSent = parseInt(BEANCHANGE_PERSENT);
 	console.log(`检测到设定了分段通知:` + intPerSent);
@@ -102,9 +115,13 @@ if ($.isNode() && process.env.BEANCHANGE_USERGP4) {
 if ($.isNode() && process.env.BEANCHANGE_DISABLECASH) {
 	DisableCash = process.env.BEANCHANGE_DISABLECASH;
 }
-if ($.isNode() && process.env.BEANCHANGE_ENABLEMONTH) {
-	EnableMonth = process.env.BEANCHANGE_ENABLEMONTH;
-}
+
+
+//取消月结查询
+//if ($.isNode() && process.env.BEANCHANGE_ENABLEMONTH) {
+	//EnableMonth = process.env.BEANCHANGE_ENABLEMONTH;
+//}
+
 if ($.isNode() && process.env.BEANCHANGE_ALLNOTIFY) {
 	
 	var strTempNotify=process.env.BEANCHANGE_ALLNOTIFY ? process.env.BEANCHANGE_ALLNOTIFY.split('&') : [];
@@ -118,12 +135,15 @@ if ($.isNode() && process.env.BEANCHANGE_ALLNOTIFY) {
 	console.log(strAllNotify);
 }
 
-if (1 == 1 && Today.getHours() > 20)
+if (EnableMonth == "true" && Today.getDate() == 1 && Today.getHours() > 17)
 	llShowMonth = true;
 
 let userIndex2 = -1;
 let userIndex3 = -1;
 let userIndex4 = -1;
+
+
+let decExBean=0;
 
 if ($.isNode()) {
 	Object.keys(jdCookieNode).forEach((item) => {
@@ -134,8 +154,10 @@ if ($.isNode()) {
 } else {
 	cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
+
+
 !(async() => {
-    console.log(`\n❗❗❗❗❗❗\n啊！注意京东过期时间\n❗❗❗❗❗❗\n`);
+    console.log(`\n❗❗❗❗❗❗\n注意:本仓库偷助力，偷CK，今天用这个仓库，明天你一觉醒来服务器就被我偷走了🌝🌝🌚🌚\n❗❗❗❗❗❗\n`);
 	if (!cookiesArr[0]) {
 		$.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {
 			"open-url": "https://bean.m.jd.com/bean/signIndex.action"
@@ -179,11 +201,12 @@ if ($.isNode()) {
 			$.jdCash = 0;
 			$.isPlusVip = 0;
 			$.JingXiang = "";
-
 			$.allincomeBean = 0; //月收入
 			$.allexpenseBean = 0; //月支出
-			$.joylevel = 0;
+			$.joylevel = 0;	
+			$.beanChangeXi=0;		
 			TempBaipiao = "";
+			strGuoqi="";
 			console.log(`******开始查询【京东账号${$.index}】${$.nickName || $.UserName}*********`);
 
 			await TotalBean();
@@ -215,7 +238,7 @@ if ($.isNode()) {
 			await requestAlgo();
 			await JxmcGetRequest();
 			await bean();
-
+			
 			if (llShowMonth) {
 				console.log("开始获取月数据，请稍后...");
 				await Monthbean();
@@ -518,9 +541,10 @@ async function showMsg() {
 	if ($.expenseBean != 0) {
 		ReturnMessage += `,支${$.expenseBean}豆`;
 	}
-	ReturnMessage += `\n`;
-	if ($.levelName || $.JingXiang){
-		ReturnMessage += `【当前京豆】${$.beanCount}豆(≈${($.beanCount / 100).toFixed(2)}元)\n`;
+	ReturnMessage += `\n`;	
+	
+	if ($.levelName || $.JingXiang){		
+		ReturnMessage += `【当前京豆】${$.beanCount}豆(≈${(($.beanCount-$.beanChangeXi)/ 100).toFixed(2)}元)\n`;		
 	} else {
 		ReturnMessage += `【当前京豆】获取失败,接口返回空数据\n`;
 	}
@@ -719,9 +743,14 @@ async function showMsg() {
 			}
 		}
 	}
+	if(strGuoqi){		
+		ReturnMessage += `💸💸💸临期京豆明细💸💸💸\n`;
+		ReturnMessage += `${strGuoqi}`;
+	}
 	ReturnMessage += `🧧🧧🧧红包明细🧧🧧🧧\n`;
 	ReturnMessage += `${$.message}`;
-
+	
+	
 	if (userIndex2 != -1) {
 		allMessageGp2 += ReturnMessageTitle+ReturnMessage + `\n`;
 	}
@@ -816,11 +845,19 @@ async function bean() {
 	}
 	$.todayOutcomeBean = -$.todayOutcomeBean;
 	$.expenseBean = -$.expenseBean;
-	//await queryexpirejingdou();//过期京豆
-	//$.todayOutcomeBean=$.todayOutcomeBean+$.expirejingdou;
-	await redPacket(); //过期红包
-	// console.log(`昨日收入：${$.incomeBean}个京豆 🐶`);
-	// console.log(`昨日支出：${$.expenseBean}个京豆 🐶`)
+	
+	decExBean =0;
+	await queryexpirejingdou();//过期京豆
+	if(decExBean && doExJxBeans=="true"){
+		var jxbeans = await exchangejxbeans(decExBean);
+		if (jxbeans) {			
+			$.beanChangeXi=decExBean;
+			console.log(`已为您将`+decExBean+`临期京豆转换成喜豆！`);
+			strGuoqi += `已为您将`+decExBean+`临期京豆转换成喜豆！\n`;
+		}
+	}
+	
+	await redPacket(); 
 }
 
 async function Monthbean() {
@@ -1172,7 +1209,6 @@ function queryexpirejingdou() {
 				"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Mobile/15E148 Safari/604.1"
 			}
 		}
-		$.expirejingdou = 0;
 		$.get(options, (err, resp, data) => {
 			try {
 				if (err) {
@@ -1182,15 +1218,14 @@ function queryexpirejingdou() {
 					if (data) {
 						// console.log(data)
 						data = JSON.parse(data.slice(23, -13));
-						// console.log(data)
-						if (data.ret === 0) {
+						if (data.ret === 0) {							
 							data['expirejingdou'].map(item => {
-								//console.log(`${timeFormat(item['time'] * 1000)}日过期京豆：${item['expireamount']}\n`);
-							})
-							$.expirejingdou = data['expirejingdou'][0]['expireamount'];
-							// if ($.expirejingdou > 0) {
-							//   $.message += `\n今日将过期：${$.expirejingdou}京豆 🐶`;
-							// }
+								if(item['expireamount']!=0){																	
+									strGuoqi+=`【${timeFormat(item['time'] * 1000)}】过期${item['expireamount']}豆\n`;
+									if (decExBean==0)
+										decExBean=item['expireamount'];
+								}
+							})							
 						}
 					} else {
 						console.log(`京东服务器返回空数据`)
@@ -1204,6 +1239,49 @@ function queryexpirejingdou() {
 			}
 		})
 	})
+}
+function exchangejxbeans(o) {
+    return new Promise(async resolve => {
+		var UUID = getUUID('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');		
+		var JXUA = `jdpingou;iPhone;4.13.0;14.4.2;${UUID};network/wifi;model/iPhone10,2;appBuild/100609;ADID/00000000-0000-0000-0000-000000000000;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`;
+        const options = {
+            "url": `https://m.jingxi.com/deal/masset/jd2xd?use=${o}&canpintuan=&setdefcoupon=0&r=${Math.random()}&sceneval=2`,
+            "headers": {
+                "Host": "m.jingxi.com",
+                "Accept": "*/*",
+                "Cookie": cookie,
+                "Connection": "keep-alive",
+                "User-Agent": JXUA,
+                "Accept-Language": "zh-cn",
+                "Referer": "https://m.jingxi.com/deal/confirmorder/main",
+                "Accept-Encoding": "gzip, deflate, br",
+            }
+        }
+        $.get(options, (err, resp, data) => {
+            try {
+                if (err) {
+                    console.log(err);
+                } else {
+                    data = JSON.parse(data);
+                    if (data && data.data && JSON.stringify(data.data) === '{}') {
+                        console.log(JSON.stringify(data))
+                    }
+                }
+            } catch (e) {
+                $.logErr(e, resp)
+            } finally {
+                resolve(data || {});
+            }
+        })
+    })
+}
+function getUUID(x = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", t = 0) {
+    return x.replace(/[xy]/g, function (x) {
+        var r = 16 * Math.random() | 0,
+        n = "x" == x ? r : 3 & r | 8;
+        return uuid = t ? n.toString(36).toUpperCase() : n.toString(36),
+        uuid
+    })
 }
 
 function redPacket() {
