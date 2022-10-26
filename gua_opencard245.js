9ce8fe57bc223137e1afac2b5c08d1d101776a68
cestjojo <46280959+anyung533@users.noreply.github.com>
2022-10-26 14:22:52 +0800

    Update gua_opencard245.js

--- gua_opencard245.js
+++ gua_opencard245.js
@@ -37,7 +37,7 @@ cron:
 let guaopencard_addSku = 'true' // 加购
 let guaopencard = 'true' // 执行
 let guaopenwait = 0 // 等待（秒）
-let guaopencard_draw = 0 // 抽奖次数
+let guaopencard_draw = 3 // 抽奖次数
 let whitelist = '' // 白名单 用&隔开 pin值(填中文
 let blacklist = '' // 黑名单 用&隔开 pin值(填中文
 
